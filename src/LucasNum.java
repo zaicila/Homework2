1e155ed03f9058ea9e8801ba8e09f9af6ad3f6fb
Alicia <azhu4@wpi.edu>
2024-11-05 13:24:08 -0500

    working lucas number function with tests in main

--- src/LucasNum.java
+++ src/LucasNum.java
@@ -1,2 +1,13 @@
 public class LucasNum {
+    public static int lucasNum(int n) {
+        // hard code case for 0 and 1
+        if (n == 0) {
+            return 2;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        // recurse
+        return lucasNum(n - 1) + lucasNum(n - 2);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // tests LucasNum class
+        System.out.println("Enter n for Lucas Number");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(LucasNum.lucasNum(i) + " ");
+        }
     }
 }
